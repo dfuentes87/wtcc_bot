b7accba681fd533ae5e368196d25ab019356b652
David Fuentes <hello@linuxsysadmin.io>
2022-03-27 18:09:41 -0400

    Fixed transfer program soup

--- transfer_info.py
+++ transfer_info.py
@@ -24,12 +24,12 @@ def embed_builder_prog(program):
     program = "^" + program
     program_title = soup.find(string=re.compile(program, re.IGNORECASE))
 
-    # title url (tbd?)
-
     if program_title is None:
         embed = discord.Embed(
             title="404: Degree/Program not found",
-            description="Check your spelling, otherwise that program/degree may not be transferable.\n[Click here](https://www.waketech.edu/programs-courses/credit/transfer-choices/by-degree) to check the full degree transfer list.",
+            description="Check your spelling, otherwise that program/degree may not be transferable.\n \
+            [Click here](https://www.waketech.edu/programs-courses/credit/transfer-choices/by-degree) \
+            to check the full degree transfer list.",
             color=WAKETECH_BLUE,
         )
         embed.set_footer(
@@ -37,19 +37,22 @@ def embed_builder_prog(program):
 
         return embed
 
+    # title url (tbd?)
+
     # program transfer options
     program_details = ""
     for item in soup.find_all("strong"):
         if program_title in str(item):
-            for next in item.find_all_next("tr"):
-                if "strong" not in str(next):
-                    next = str(next)
-                    next = re.sub('<.*?>','',next, flags=re.DOTALL)
-                    details = next.replace('\n','** : ',1)
-                    details = "**" + details
+            for children in item.find_all_next("tr"):
+                if "strong" not in str(children):
+                    children = str(children)
+                    children = re.sub('<.?tr>','',children)
+                    children = re.sub('<.?td>', '**', children, 2)
+                    children = re.sub('<.?td>', '', children, 2)
+                    children = re.sub('\n', '', children, 1)
                     # remove annoying HTML 'amp;'
-                    details = details.replace('amp;','')
-                    program_details += details + "\n"
+                    children = children.replace('amp;','')
+                    program_details += children + "\n"
                 else:
                     break
 
@@ -58,7 +61,7 @@ def embed_builder_prog(program):
     # create the embed
     embed = discord.Embed(
         title="TRANSFER OPTIONS FOR: \n" + program_title,
-        #url=url,
+        # url=url,
         description=program_details,
         color=WAKETECH_BLUE,
     )
@@ -67,44 +70,56 @@ def embed_builder_prog(program):
     return embed
 
 
-def embed_builder_uni(university):
+def embed_builder_uni(university, transfer_link=None):
     url = 'https://www.waketech.edu/programs-courses/credit/transfer-choices/search-college'
     page = requests.get(url)
-    content = page.content
-    soup = BeautifulSoup(content, 'lxml')
+    # get rid of everything but the universities
+    uni_list = SoupStrainer(class_="paragraph--view-mode--default panel-heading faq")
+    soup = BeautifulSoup(page.content, "lxml", parse_only=uni_list)
 
-    # university name
-    ## UNIVERSITYNAME
+    # '^' will make regex do a 'starts with' search
+    university = "^" + university
+    university_name = soup.find(string=re.compile(university, re.IGNORECASE))
 
-    # url (tbd..anchor?)
+    if university_name is None:
+        embed = discord.Embed(
+            title="404: University not found",
+            description="Keep in mind only NC universities are listed, or you may want to check your spelling.\n \
+            [Click here](https://www.waketech.edu/programs-courses/credit/transfer-choices/search-college#) \
+            to see the full list of NC universities.",
+            color=WAKETECH_BLUE,
+        )
+        embed.set_footer(
+            text="Questions, suggestions, or problems regarding the bot? Send a message to netdragon#3288")
 
-    # description
-    ## affiliation & list of degrees/programs
+        return embed
+
+    # title url (tbd..anchor?)
+
+    # list of degrees
 
     # transfer link
 
-
     # aas transfer link
-
 
     # transfer degree plans link (including AE)
 
-
     # talk with advisor url
-
 
     # create the embed
     embed = discord.Embed(
-        title=university_name,
-        #url=url,
-        description=program_description,
+        title="TRANSFER OPTIONS FOR: \n" + university_name,
+        # url=url,
+        description=degree_list,
         color=WAKETECH_BLUE,
     )
-    embed.add_field(name="Transfer Link", value=reqs, inline=True)
-    embed.add_field(name="AAS Transfer Link", value=reqs, inline=True)
-    embed.add_field(name="Transfer Degree Plans", value=reqs, inline=True)
-    if advisor_url is not None:
-        embed.add_field(name="Talk with a University Advisor", value=advisor_url, inline=True)
-
+    if transfer_link is not None:
+        embed.add_field(name="Transfer Link", value=reqs, inline=True)
+    if aas_link is not None:
+        embed.add_field(name="AAS Transfer Link", value=reqs, inline=True)
+    if degree_plan is not None:
+        embed.add_field(name="Transfer Degree Plans", value=reqs, inline=True)
+    if advisor_link is not None:
+        embed.add_field(name="Talk with a University Advisor", value=advisor_link, inline=True)
 
     return embed
